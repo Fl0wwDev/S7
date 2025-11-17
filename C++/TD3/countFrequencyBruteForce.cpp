f6f08c1f12c4aeed595e1811da2835649ddc1607
Fl0wwDev <mafradin@hotmail.fr>
2025-11-17 14:36:02 +0100

    Add function definitions and constructors for frequency counting classes

--- C++/TD3/countFrequencyBruteForce.cpp
+++ C++/TD3/countFrequencyBruteForce.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include "countFrequencyBruteForce.h"
 using namespace std;
 
 void countFrenquencyBruteForce(const vector<int>& numbers) {
+}
+countFrequencyBruteForce::countFrequencyBruteForce()
+{
 }
--- C++/TD3/countFrequencyOptimal.cpp
+++ C++/TD3/countFrequencyOptimal.cpp
@@ -1,5 +1,10 @@
 #include <map>
+#include "countFrequencyOptimal.h"
 using namespace std;
 
 map<int, int> countFrequencyOptimal(const vector<int>&numbers){
+}
+
+countFrequencyOptimal::countFrequencyOptimal()
+{
 }
