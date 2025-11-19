f23e47a0764356749e23b739923819f2a607365f
Fl0wwDev <mafradin@hotmail.fr>
2025-11-19 11:42:59 +0100

    Refactor code structure for improved readability and maintainability

--- C++/TD3/exercice2/main.cpp
+++ C++/TD3/exercice2/main.cpp
@@ -5,9 +5,30 @@
 using namespace std;
 
 vector<int> twoSumBruteForce(const vector<int>& nums, int target){
+    for (size_t i = 0; i < nums.size(); ++i) {
+        for (size_t j = i + 1; j < nums.size(); ++j) {
+            if (nums[i] + nums[j] == target) {
+                return {static_cast<int>(i), static_cast<int>(j)};
+            }
+        }
+    }
+    return {};
 }
 
 vector<int> twoSumOptimal(const vector<int>& nums, int target){
+    unordered_map<int, int> numMap;
+    
+    for (size_t i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+        
+        if (numMap.find(complement) != numMap.end()) {
+            return {numMap[complement], static_cast<int>(i)};
+        }
+        
+        numMap[nums[i]] = i;
+    }
+    
+    return {};
 }
 
 int main(int argc, char const *argv[])
@@ -18,9 +39,9 @@ int main(int argc, char const *argv[])
     int target = 9;
 
     vector<int> indicesBruteForce = twoSumBruteForce(nums, target);
-    cout << "Brute Force Solution:"
-         << "indicesBruteForce[0]"
-         << ","
+    cout << "Brute Force Solution: ["
+         << indicesBruteForce[0]
+         << ", "
          << indicesBruteForce[1]
          << "]"
          << endl;
