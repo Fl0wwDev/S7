4d5875298f6bb128e66ac36c69ef269327e3c581
Fl0wwDev <mafradin@hotmail.fr>
2025-11-19 11:39:00 +0100

    Add month printing functionality in 1.8.c and remove unused PDF file

--- Programmation Système/Thème 1/1.8.c
+++ Programmation Système/Thème 1/1.8.c
@@ -8,6 +8,9 @@ int main(int argc, char const *argv[])
 
     int mois[12] = {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"};
 
+    for(int i=0; i < 12; i++){
+        prinf("%s", mois[i]);
+    }
     scanf("%[^\n]%*c", str);
     printf("%s", str);
     return 0;
