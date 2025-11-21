bfcb69ab1d20bca3b9f1b4dc3c2f816264bd8873
Fl0wwDev <mafradin@hotmail.fr>
2025-11-21 15:58:23 +0100

    Enhance Pokémon type distribution visualization and clean up code structure

--- Programmation Système/Thème 1/1.8.c
+++ Programmation Système/Thème 1/1.8.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
+struct mois {
+    char nom[10];  
+    int jours; 
+};
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    char str[20];
-
-    int mois[12] = {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"};
-
-    for(int i=0; i < 12; i++){
-        prinf("%s", mois[i]);
+int main() {
+    const struct mois mois_annee[12] = {
+        {"janvier", 31},
+        {"fevrier", 28},
+        {"mars", 31},
+        {"avril", 30},
+        {"mai", 31},
+        {"juin", 30},
+        {"juillet", 31},
+        {"aout", 31},
+        {"septembre", 30},
+        {"octobre", 31},
+        {"novembre", 30},
+        {"decembre", 31}
+    };
+    
+    char saisie[20];
+    int trouve = 0;
+    
+    printf("met numero mois");
+    scanf("%s", saisie);
+    
+    for (int i = 0; i < 12; i++) {
+        if (strcmp(saisie, mois_annee[i].nom) == 0) {
+            printf("mois : %d\n", i + 1);
+            printf("jours : %d\n", mois_annee[i].jours);
+            trouve = 1;
+            break;
+        }
     }
-    scanf("%[^\n]%*c", str);
-    printf("%s", str);
+    
+    if (!trouve) {
+        printf("marche po\n");
+    }
+    
     return 0;
 }
