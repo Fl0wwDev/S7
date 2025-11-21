e3f267f683a5708e55d1cc328d7fa500f006ec11
Fl0wwDev <mafradin@hotmail.fr>
2025-11-21 09:10:08 +0100

    Remove unused import of classification_report from sklearn.metrics

--- Fouille de Données/TP1/kaggle.py
+++ Fouille de Données/TP1/kaggle.py
@@ -7,7 +7,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix
 import warnings
 warnings.filterwarnings('ignore')
 
