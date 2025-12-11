124adfd4c04b98689705388814e11c35ac83e2dc
Fl0wwDev <mafradin@hotmail.fr>
2025-12-11 15:27:11 +0100

    Add signal handling functions and pid display in 9.4.c

--- Programmation Système/Theme/Theme9/9.4.c
+++ Programmation Système/Theme/Theme9/9.4.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 
+pid_t;
+pid_t_fils; 
+
+void traite(){
+    printf("Le pid:", getpid());
+}
 
 void sigurs1(int signal){
     printf("signal sigurs1");
