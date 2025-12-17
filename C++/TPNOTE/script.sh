ec39644dd6058cbc3a3c0f6972e46e180fef5cbb
Fl0wwDev <mafradin@hotmail.fr>
2025-12-17 11:57:31 +0100

    script

--- C++/TPNOTE/script.sh
+++ C++/TPNOTE/script.sh
@@ -1,1 +1,1 @@
- g++ main.cpp networkDevice.cpp networkInfrastructure.cpp router.cpp server.cpp switch.cpp -o  main
+g++ main.cpp networkDevice.cpp networkInfrastructure.cpp router.cpp server.cpp knnDouble.cpp  switch.cpp -o  main
